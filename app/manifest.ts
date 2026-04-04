70ea2abaf43e35923a245a53dcdf9f092d5265e7
rajdeep546 <rajdeepsinghchavda7@gmail.com>
2026-04-04 12:01:46 +0530

    icons

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,12 +11,17 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#f97316",
     icons: [
       {
-        src: "/icon-192.png",
+        src: "/favicon-48x48.png",
+        sizes: "48x48",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-192x192.png",
         sizes: "192x192",
         type: "image/png",
       },
       {
-        src: "/icon-512.png",
+        src: "/android-chrome-512x512.png",
         sizes: "512x512",
         type: "image/png",
       },
